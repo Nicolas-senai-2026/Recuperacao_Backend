f5ad60003b98a8a18d50f5c7b6aec32d3c578232
Nicolas-senai-2026 <dichava190@gmail.com>
2026-09-23 09:28:18 -0300

    sexto_commit

--- rec06_media.js
+++ rec06_media.js
@@ -7,3 +7,16 @@
 // ☐ Exibir a soma dos tempos e a média.
 
 const entrada = require('readline-sync');
+
+let acumulador = 0;
+
+for (let tempo = 1; tempo <= 6; tempo++) {
+    const tempo = entrada.questionFloat("Digite o tempo: ");
+    acumulador += tempo;
+}
+
+const media = acumulador / 6;
+
+console.log("\n=== RESULTADO DA MEDIA ===");
+console.log(`Soma do acumulador ${acumulador}`);
+console.log(`Média final: ${media}`);
