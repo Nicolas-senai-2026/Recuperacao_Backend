da7f83312556c890fe52af4cc887e753cc63e8d5
Nicolas-senai-2026 <dichava190@gmail.com>
2026-09-23 08:58:20 -0300

    segundo_commit

--- rec02_materiais.js
+++ rec02_materiais.js
@@ -8,3 +8,18 @@
 // ☐ Exibir um resumo contendo nome, quantidade, preço unitário e total
 
 const entrada = require('readline-sync');
+
+const nomePeca = entrada.question("Nome da peca: ");
+
+const QntdComprada = entrada.questionInt("Quantidade de pecas compradas: ");
+
+const precoUnitario = entrada.questionFloat("Preco da peca: ");
+
+const valorTotal = precoUnitario * QntdComprada;
+
+
+console.log("\n=== RESUMO DA COMPRA ===");
+console.log(`Nome da peca: ${nomePeca}`);
+console.log(`Quantidade comprada: ${QntdComprada.toFixed(2)}`);
+console.log(`Preço unitário: R$ ${precoUnitario.toFixed(2)}`);
+console.log(`Valor total: R$ ${valorTotal.toFixed(2)}`);
